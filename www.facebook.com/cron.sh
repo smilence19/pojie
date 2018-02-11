ec639f7ff709aa46f23bad927011ea9fbabdc6d5
huangqingjun <huangqingjunqwe@126.com>
2018-02-11 15:17:52 +0800

    facebook

--- www.facebook.com/cron.sh
+++ www.facebook.com/cron.sh
@@ -4,6 +4,8 @@ export PATH=$PATH:/usr/local/bin
 #进入.py脚本所在目录
 cd /www.facebook.com
 
+sudo -s
+
 #执行.py中定义的项目example，并指定日志文件，其中nohup....&表示可以在后台执行，不会因为关闭终端而导致程序执行中断。
 nohup sudo scrapy crawl facebook_post_mainpage_dairly >> facebook_post_mainpage_dairly.log 2>&1 &
 
